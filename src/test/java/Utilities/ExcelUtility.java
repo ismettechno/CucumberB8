0126aec893ea17275916bcd3d6beae0936dbfc5a
TechnoStudy <ismettechno@gmail.com>
2025-11-20 19:51:03 +0000

    ExcelUtility getData

--- src/test/java/Utilities/ExcelUtility.java
+++ src/test/java/Utilities/ExcelUtility.java
@@ -1,4 +1,38 @@
 package Utilities;
 
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExcelUtility {
+
+    public static List<List<String>> getData(String path, String sheetName, int sutunSayisi) {
+        List<List<String>> tablo=new ArrayList<>();
+        Sheet sheet=null;
+
+        try {
+            FileInputStream inputStream = new FileInputStream(path);
+            Workbook workbook = WorkbookFactory.create(inputStream);
+            sheet = workbook.getSheet(sheetName);
+        }
+        catch (Exception ex)
+        {
+            System.out.println("ex.getMessage() = " + ex.getMessage());
+        }
+
+        for (int i = 0; i < sheet.getPhysicalNumberOfRows(); i++) {
+            List<String> satir=new ArrayList<>();
+            for (int j = 0; j < sutunSayisi ; j++)
+                satir.add(sheet.getRow(i).getCell(j).toString());
+
+            tablo.add(satir);
+        }
+
+        return tablo;
+    }
+
 }
