5031d7e0df097f7470e58851becadcc5ca515e91
TechnoStudy <ismettechno@gmail.com>
2025-11-20 19:18:39 +0000

    _10_Soru çözüm

--- src/test/java/ApachePOI/_10_Soru.java
+++ src/test/java/ApachePOI/_10_Soru.java
@@ -13,5 +13,61 @@ package ApachePOI;
 // Merhaba Dünya 3
 // ...
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class _10_Soru {
+
+    public static void main(String[] args) throws IOException {
+
+        String path= "src/test/java/ApachePOI/Resource/YeniExcel2.xlsx";
+
+        File dosya=new File(path);
+        if (!dosya.exists())  // dosya yok ise
+        {
+            XSSFWorkbook workbook=new XSSFWorkbook();
+            XSSFSheet sheet= workbook.createSheet("Sheet1");
+
+            Row yeniSatir= sheet.createRow(0);
+            Cell yeniHucre= yeniSatir.createCell(0);
+            yeniHucre.setCellValue("Merhaba Dünya 0");
+
+            // Kaydet
+            FileOutputStream outputStream=new FileOutputStream(path); // yazma yönünde
+            workbook.write(outputStream);
+            workbook.close();
+            outputStream.close();
+
+            System.out.println("Yeni dosya oluşturuldu ve ilk satıra yazıldı");
+        }
+        else
+        {
+            // var olan dosyaya ekle
+            FileInputStream inputStream=new FileInputStream(path);
+            Workbook workbook= WorkbookFactory.create(inputStream);
+            Sheet sheet= workbook.getSheetAt(0);
+
+            int varOlanSatirSayisi= sheet.getPhysicalNumberOfRows(); // var satır sayısı
+            Row yeniSatir= sheet.createRow(varOlanSatirSayisi); // yeni en sona satır açtım
+            Cell yeniHucre=yeniSatir.createCell(0);
+            yeniHucre.setCellValue("Merhaba Dünya "+varOlanSatirSayisi);
+
+            inputStream.close();// okuma modunu kapat
+
+            // Kaydet
+            FileOutputStream outputStream=new FileOutputStream(path); // yazma yönünde
+            workbook.write(outputStream);
+            workbook.close();
+            outputStream.close();
+
+            System.out.println("Var olan dosyaya eklendi");
+        }
+
+    }
 }
