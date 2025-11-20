8e426b4b168c1d1d5db233c5b44d099076ee82b1
TechnoStudy <ismettechno@gmail.com>
2025-11-20 19:25:34 +0000

    _11_Soru soru kısmı

---
+++ src/test/java/ApachePOI/_11_Soru.java
@@ -0,0 +1,11 @@
+package ApachePOI;
+
+/*
+   Main den bir metod çağırmak suretiyle, path i ve sheetName i verilen excelden
+   istenilen sütun kadar veriyi okuyup bir List e atınız.
+   Bu soruda kaynak Excel için : EditAccount.xlsx  in 2.sheet ini kullanabilirsiniz.
+ */
+
+public class _11_Soru {
+
+}
