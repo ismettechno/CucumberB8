c4ea39f0c3c4107a6a8748de36032c58d68978a8
TechnoStudy <ismettechno@gmail.com>
2025-11-20 20:21:52 +0000

    _12_Soru Çözüm

--- src/test/java/ApachePOI/_12_Soru.java
+++ src/test/java/ApachePOI/_12_Soru.java
@@ -1,5 +1,13 @@
 package ApachePOI;
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 /*
    Main den bir metod çağırmak suretiyle, henüz oluşturulmamış
    path i verilen excel için, program her çalıştıkça içine "test passed"
@@ -22,8 +30,8 @@ else
  */
 public class _12_Soru {
     public static void main(String[] args) {
-        String path="src/test/java/ApachePOI/resource/TestSonuclari.xlsx";
-        String senaryoAdi="Create Country";
+        String path="src/test/java/ApachePOI/Resource/TestSonuclari.xlsx";
+        String senaryoAdi="Edit Account";
         String testSonuc="Test Passed";
 
         writeToExcel(path, senaryoAdi, testSonuc); //excel create
@@ -31,6 +39,57 @@ public class _12_Soru {
         writeToExcel(path, senaryoAdi, testSonuc); //dosya artık var ekleyecek
         writeToExcel(path, senaryoAdi, testSonuc); //dosya artık var ekleyecek
         writeToExcel(path, senaryoAdi, testSonuc); //dosya artık var ekleyecek
+    }
+
+    public static void writeToExcel(String path, String senaryoAdi, String testSonuc)
+    {
+        try {
+            File dosya = new File(path);
+            if (!dosya.exists())  // dosya yok ise
+            {
+                XSSFWorkbook workbook = new XSSFWorkbook();
+                XSSFSheet sheet = workbook.createSheet("Sheet1");
+
+                Row yeniSatir = sheet.createRow(0);
+                Cell hucre1 = yeniSatir.createCell(0); hucre1.setCellValue(senaryoAdi);
+                Cell hucre2 = yeniSatir.createCell(1); hucre2.setCellValue(testSonuc);
+
+                // Kaydet
+                FileOutputStream outputStream = new FileOutputStream(path); // yazma yönünde
+                workbook.write(outputStream);
+                workbook.close();
+                outputStream.close();
+
+                System.out.println("Yeni dosya oluşturuldu ve ilk satıra yazıldı");
+            } else {
+                // var olan dosyaya ekle
+                FileInputStream inputStream = new FileInputStream(path);
+                Workbook workbook = WorkbookFactory.create(inputStream);
+                Sheet sheet = workbook.getSheetAt(0);
+
+                int varOlanSatirSayisi = sheet.getPhysicalNumberOfRows(); // var satır sayısı
+                Row yeniSatir = sheet.createRow(varOlanSatirSayisi); // yeni en sona satır açtım
+                Cell hucre1 = yeniSatir.createCell(0); hucre1.setCellValue(senaryoAdi);
+                Cell hucre2 = yeniSatir.createCell(1); hucre2.setCellValue(testSonuc);
+
+                inputStream.close();// okuma modunu kapat
+
+                // Kaydet
+                FileOutputStream outputStream = new FileOutputStream(path); // yazma yönünde
+                workbook.write(outputStream);
+                workbook.close();
+                outputStream.close();
+
+                System.out.println("Var olan dosyaya eklendi");
+            }
+        }
+        catch (Exception ex)
+        {
+            System.out.println("ex.getMessage() = " + ex.getMessage());
+        }
 
     }
+
+
+
 }
--- src/test/java/Utilities/ExcelUtility.java
+++ src/test/java/Utilities/ExcelUtility.java
@@ -1,10 +1,12 @@
 package Utilities;
 
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,5 +36,55 @@ public class ExcelUtility {
 
         return tablo;
     }
+
+    public static void writeToExcel(String path, String senaryoAdi, String testSonuc)
+    {
+        try {
+            File dosya = new File(path);
+            if (!dosya.exists())  // dosya yok ise
+            {
+                XSSFWorkbook workbook = new XSSFWorkbook();
+                XSSFSheet sheet = workbook.createSheet("Sheet1");
+
+                Row yeniSatir = sheet.createRow(0);
+                Cell hucre1 = yeniSatir.createCell(0); hucre1.setCellValue(senaryoAdi);
+                Cell hucre2 = yeniSatir.createCell(1); hucre2.setCellValue(testSonuc);
+
+                // Kaydet
+                FileOutputStream outputStream = new FileOutputStream(path); // yazma yönünde
+                workbook.write(outputStream);
+                workbook.close();
+                outputStream.close();
+
+                System.out.println("Yeni dosya oluşturuldu ve ilk satıra yazıldı");
+            } else {
+                // var olan dosyaya ekle
+                FileInputStream inputStream = new FileInputStream(path);
+                Workbook workbook = WorkbookFactory.create(inputStream);
+                Sheet sheet = workbook.getSheetAt(0);
+
+                int varOlanSatirSayisi = sheet.getPhysicalNumberOfRows(); // var satır sayısı
+                Row yeniSatir = sheet.createRow(varOlanSatirSayisi); // yeni en sona satır açtım
+                Cell hucre1 = yeniSatir.createCell(0); hucre1.setCellValue(senaryoAdi);
+                Cell hucre2 = yeniSatir.createCell(1); hucre2.setCellValue(testSonuc);
+
+                inputStream.close();// okuma modunu kapat
+
+                // Kaydet
+                FileOutputStream outputStream = new FileOutputStream(path); // yazma yönünde
+                workbook.write(outputStream);
+                workbook.close();
+                outputStream.close();
+
+                System.out.println("Var olan dosyaya eklendi");
+            }
+        }
+        catch (Exception ex)
+        {
+            System.out.println("ex.getMessage() = " + ex.getMessage());
+        }
+
+    }
+
 
 }
