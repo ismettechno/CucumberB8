827fd63e48c2bc9f3609fc2afe6905e2ca130fc1
TechnoStudy <ismettechno@gmail.com>
2025-12-08 20:07:53 +0000

    JDBC _03_NextPrevAbsRel özet

--- src/test/java/_JDBC/_03_NextPrevAbsRel.java
+++ src/test/java/_JDBC/_03_NextPrevAbsRel.java
@@ -19,8 +19,32 @@ public class _03_NextPrevAbsRel extends JDBCParent{
 
         rs.previous();  // 1 adım geri, onceki
         System.out.println("1.Satır = " + rs.getString("name")); // English
+
+        rs.first(); // ilk satıra git
+        System.out.println("İlk Satır = " + rs.getString("name")); // English
+
+        rs.last(); // son satıra git
+        System.out.println("Son Satır = " + rs.getString("name")); // German
+        int sonSatirMik= rs.getRow();  // bulunduğun satırın kaçıncı
+        System.out.println("sonSatirMik = " + sonSatirMik);
+
+        rs.absolute(4);  // direk baştan sayarak 4.satıra gider
+        System.out.println("4.Satır = " + rs.getString("name")); // Mandarin
+
+        rs.relative(2);  // bulunduğun yerden sayarak 2.satıra gider
+        System.out.println("6.Satır = " + rs.getString("name")); // German
+
         DBConnectionClose();
     }
 
-
+// rs.next() : sonraki satır
+// rs.previous : önceki satır
+// rs.absolute(10) : baştan itibaren 10.satıra ileri gider.
+// rs.relative(10) : bulunduğu yerden 10.satıra ileri gider.
+// rs.absolute(-10) : - (eksi) sondan demektir, sondan 10.satır
+// rs.relative(-5) :  - (eksi) bulunduğu yerden 5 satır geri
+// ilk satırdan relative ile 5 geri gitmek istersek sınırı taştığı için hata verecektir
+// rs.first() :  ilk satıra git
+// rs.last() :  Son satıra git
+// rs.getRow(); bulunduğu satırın kaçıncı satır bilgisini veririr
 }
