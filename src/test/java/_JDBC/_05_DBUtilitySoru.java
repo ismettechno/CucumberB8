dfe2047dbf8b86a48a28b16c470d9fe19736c2a8
TechnoStudy <ismettechno@gmail.com>
2025-12-08 20:48:32 +0000

    DBUtility

--- src/test/java/_JDBC/_05_DBUtilitySoru.java
+++ src/test/java/_JDBC/_05_DBUtilitySoru.java
@@ -1,19 +1,72 @@
 package _JDBC;
 
+import java.sql.*;
 import java.util.ArrayList;
 
-public class _05_DBUtilitySoru extends JDBCParent{
+public class _05_DBUtilitySoru {
 
     // Gönderilen sorgu sonucundaki tüm datayı bir List olarak döndüren metodu yazınız.
     public static void main(String[] args) {
 
-        String sorguSql="select first_name, last_name from customer";
+        String sorguSql="select film_id,title,release_year from film";
         ArrayList<ArrayList<String>> donenData= getListData(sorguSql);
 
+        System.out.println("donenData = " + donenData);
     }
 
+    public static ArrayList<ArrayList<String>> getListData(String sorguSql)
+    {
+        ArrayList<ArrayList<String>> tablo=new ArrayList<>();
+        DBConnectionOpen();
 
-    // metodu yazınız
+        try {
+            ResultSet rs = sorguEkrani.executeQuery(sorguSql);
+            ResultSetMetaData rsmd = rs.getMetaData();
 
+            while (rs.next()) {
+                ArrayList<String> satir = new ArrayList<>();
+                for (int i = 1; i <= rsmd.getColumnCount(); i++)
+                    satir.add(rs.getString(i));
+
+                tablo.add(satir);
+            }
+        }
+        catch (Exception ex)
+        {
+            System.out.println("ex.getMessage() = " + ex.getMessage());
+        }
+
+        DBConnectionClose();
+        return  tablo;
+    }
+
+    public static Connection baglanti;
+    public static Statement sorguEkrani;
+
+    public static void DBConnectionOpen(){
+        String serverUrl = "jdbc:mysql://35.224.183.228:3306/sakila";
+        String username = "elamysqluser";
+        String password = ")KH\\Z;,sPIJvG>1$";
+
+        try {
+            baglanti = DriverManager.getConnection(serverUrl, username, password);
+            sorguEkrani = baglanti.createStatement();
+        }
+        catch (Exception ex)
+        {
+            System.out.println("ex.getMessage() = " + ex.getMessage());
+        }
+    }
+
+    public static void DBConnectionClose()
+    {
+        try {
+            baglanti.close();
+        }
+        catch (Exception ex)
+        {
+            System.out.println("ex.getMessage() = " + ex.getMessage());
+        }
+    }
 
 }
