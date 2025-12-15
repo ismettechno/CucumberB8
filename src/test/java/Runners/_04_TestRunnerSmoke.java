ecfc60ff70d8b92bdbfbd3a7c4762063dbbb3676
TechnoStudy <ismettechno@gmail.com>
2025-12-15 21:00:50 +0000

    Jenkins için JSON sonuç plugini eklendi 2

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
         tags= "@SmokeTest",
         features = {"src/test/java/FeatureFiles"}, // hepsi demek
         glue ={"StepDefinitions"}
-        ,plugin= {"json:target/cucumber/cucumber.json"} // Jenkins için JSON report
+        ,plugin= {"json:target/cucumber/cucumber.json"} // Je nkins için JSON report
 )
 public class _04_TestRunnerSmoke extends AbstractTestNGCucumberTests {
 }
